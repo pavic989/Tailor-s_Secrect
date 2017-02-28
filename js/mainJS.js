7bc63285c06dd37f1c4df3d07297c38ad6dbbd80
pavic989 <pavicmilos989@gmail.com>
2017-02-28 23:02:40 +0100

    Almost done

--- js/mainJS.js
+++ js/mainJS.js
@@ -19,3 +19,47 @@ $(document).ready(function(){
     $("#display").load($(this).attr('href'));
   });
 });
+
+// Ajax
+
+function sleep(){
+    i++;
+    if( i > 2 ){
+      i = 0;
+      ar = makeText(); // reset
+    }
+    scroll.innerHTML = '';
+    start();
+}
+
+function makeText(){
+  var text = "Sublime Text";
+  var text1 = "Brackets";
+  var text2 = "Atom";
+
+  var stext = text.split('');
+  var stext1 = text1.split('');
+  var stext2 = text2.split('');
+  var nasArray = [stext, stext1, stext2];
+  return nasArray;
+}
+
+
+var ar = makeText();
+var scroll = document.getElementById('scroll');
+var loop;
+var i = 0;
+
+
+function start(){
+  if(ar[i].length > 0){
+    scroll.innerHTML += ar[i].shift();
+    loop = setTimeout(start,400);  // speed settings
+  } else {
+    var ss = setTimeout(sleep , 2000);
+  }
+}
+
+start();
+
+// Animate #first
